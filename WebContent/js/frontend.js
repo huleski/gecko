fae2d2bb9a04fe31cca09b57e3411c8814f2aef8
huleski <huleski@gmail.com>
2017-09-07 11:53:05 +0800

    initialize repository

---
+++ WebContent/js/frontend.js
@@ -0,0 +1,62 @@
+function snapscreen() {
+	html2canvas($("body"), {
+		onrendered: function(canvas) {
+			var url = canvas.toDataURL(); //base64数据  
+			var image = new Image();
+			image.src = url;
+			image.width = 530;
+			image.height = 280;
+			$("#showScreen").html("");
+			$("#showScreen").append(image);
+		}
+	});
+}
+
+/*function showApp() {
+	if($registerOut[0] != undefined) {
+		$registerOut.hide();
+	} else {
+		$mainDiv.hide();
+	};
+	$end.hide();
+	$appbg.show();
+}
+
+function hideApp() {
+	if($registerOut[0] != undefined) {
+		$registerOut.show();
+	} else {
+		$mainDiv.show();
+	};
+	$end.show();
+	$appbg.hide();
+}
+
+function checkPassword(password) {
+	var flag = /^\w{6,}$/.test(password);
+	if(!flag) {
+		$("#passwordImply").show();
+		return false;
+	} else {
+		$("#passwordImply").hide();
+		return true;
+	}
+}
+
+function checkPhone(phoneNumber) {
+	var flag = /^1[34578]\d{9}$/.test(phoneNumber);
+	if(!flag) {
+		$("#phoneImply").show();
+		return false;
+	} else {
+		$("#phoneImply").hide();
+		return true;
+	}
+}*/
+
+/*function checkForm() {
+	var phonenum = $("#phonenumber").val();
+	var pw = $("#password").val();
+
+	return checkPhone(phonenum) && checkPassword(pw);
+}*/
